116020a4efc74e5d45b1b3ae5f3d5f086c765ef0
Manny332 <ndigidemosinachi@gmail.com>
2026-04-24 00:08:06 +0100

    Modified the frontend code

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'http://4.207.121.45:8080',
+    baseURL:'http//51.21.253.59:8080',
     headers: {
         'Content-Type': 'application/json',
     },
